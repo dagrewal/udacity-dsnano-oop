824c4e68987865757ef686ffe915a0d82d2264fb
Daniel Grewal <daniel.grewal1503@icloud.com>
2020-06-23 17:28:46 +0100

    Packaged code and uploaded to pypi.org

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup
 
-setup(name='distributions',
+setup(name='dgds_distributions',
       version='0.1',
-      description='Gaussian distributions',
-      packages=['distributions'],
+      description='Gaussian and Binomial distributions',
+      packages=['dgds_distributions'],
+      author='Daniel Grewal',
+      author_email='daniel.grewal1503@icloud.com',
       zip_safe=False)
